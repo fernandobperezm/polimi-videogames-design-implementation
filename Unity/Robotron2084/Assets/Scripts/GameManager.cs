6c0b35ec1823bc8f65b42e376bfc013e238e9a87
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-10-18 14:34:36 +0200

    Added Professor's Robotron2084 and some of myself.

---
+++ Unity/Robotron2084/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance = null;
+
+	public GameObject m_shot;
+	public GameObject m_grunt;
+	public GameObject m_grunt_explodes;
+
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		ObjectPoolingManager.Instance.CreatePool (m_shot, 100, 100);
+		ObjectPoolingManager.Instance.CreatePool (m_grunt, 100, 100);
+		ObjectPoolingManager.Instance.CreatePool (m_grunt_explodes, 100, 100);
+
+		for (int i = 0; i < 10; i++) {			
+			GameObject go = ObjectPoolingManager.Instance.GetObject (m_grunt.name);
+
+			float a = Random.Range (0, Mathf.PI * 2);
+			float r = Random.Range (80f, 150f);
+
+			Vector3 np = new Vector3 ();
+			np.x = Mathf.Cos (a) * r;
+			np.y = Mathf.Sin (a) * r;
+			np.z = transform.position.z;
+
+			go.transform.position = np;
+			go.transform.rotation = Quaternion.identity;
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Robotron2084/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+
+	public static SoundManager Instance = null;
+
+	public AudioSource m_background_music;
+
+	[Header("Player Sound Effects")]
+	public AudioSource m_player_effects;
+	public AudioClip m_shoot_audioclip;
+	public AudioClip m_grunt_explodes_audioclip;
+
+
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	public void PlayerShoots() {
+		m_player_effects.PlayOneShot (m_shoot_audioclip);
+	}
+
+	public void GruntExplodes() {
+		m_player_effects.PlayOneShot (m_grunt_explodes_audioclip);
+	}
+}
