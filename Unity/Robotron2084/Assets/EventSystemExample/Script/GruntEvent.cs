5b227ed9f748643c12947f11cec8136646d6f8fe
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-10-18 17:01:25 +0200

    Event Manager.

---
+++ Unity/Robotron2084/Assets/EventSystemExample/Script/GruntEvent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GruntEvent : MonoBehaviour {
+	public GameObject m_grunt_explosion;
+
+	// Use this for initialization
+	void OnEnable () {
+		EventManager.StartListening ("Explode",Explode);
+		EventManager.StartListening ("RunAway",RunAway);
+	}
+
+	void Explode () {
+		EventManager.StopListening ("Explode", Explode);
+		Debug.Log ("Explode");
+		StartCoroutine (ExplodeCoroutine ());
+	}
+
+	void RunAway () {
+		EventManager.StopListening ("RunAway", RunAway);
+		Debug.Log ("RunAway");
+	}
+
+	IEnumerator ExplodeCoroutine() {
+		yield return new WaitForSeconds (Random.value * 1 + 0.2f);
+		Instantiate (m_grunt_explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+		
+}
