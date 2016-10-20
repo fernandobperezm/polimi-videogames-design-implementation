fab835e55821e2992bf28d722e07c16966eaec8b
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-10-20 08:33:06 +0200

    Added Shots as prefabs.

---
+++ Unity/Robotron 2084_FernandoPerez/Assets/Scripts/Shot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+	Transform tr;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform> () as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
