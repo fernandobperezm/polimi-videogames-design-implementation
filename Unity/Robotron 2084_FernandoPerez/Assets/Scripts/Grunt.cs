6817bf8a565a3c8f84a0f2cc2ab196ae24b9fcbd
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-10-21 16:32:38 +0200

    Grunts done, next: grunt explotion.

---
+++ Unity/Robotron 2084_FernandoPerez/Assets/Scripts/Grunt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grunt : MonoBehaviour {
+
+	Transform tr;
+
+	GameObject player;
+
+	[Header ("Grunt Speed")]
+	[Range(25f,200f)]
+	public float m_speed = 25f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		tr = GetComponent<Transform> () as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// To chase the player, we must know its position to follow that direction.
+		Vector3 direction = 
+			(player.transform.position - tr.position).normalized;
+
+		// Following the player.
+		tr.position += direction * m_speed * Time.deltaTime;
+		
+	}
+}
