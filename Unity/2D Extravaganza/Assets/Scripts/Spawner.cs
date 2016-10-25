f280a2613752869e997f53ed103e151550de1429
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-10-25 17:11:13 +0200

    Added area efectors.

---
+++ Unity/2D Extravaganza/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject m_coin;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnCoins());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator SpawnCoins () {
+		for (int i = 0; i < 200; i++) {
+			Vector3 p = new Vector3 ();
+			p.x = Random.Range (-9f, 9f);
+			p.y = gameObject.transform.position.y;
+			p.z = gameObject.transform.position.z;
+			Instantiate (m_coin, p, Quaternion.identity);
+			yield return new WaitForSeconds (Random.Range (0f,0.2f));
+
+			yield return null; // This line avoids Crashes of the Coroutine and therefore, Unity crashes :D
+		}
+	}
+}
